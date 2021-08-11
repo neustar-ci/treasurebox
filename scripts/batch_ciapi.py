46f9cba6f933bbcc42982a8ea25e01c0ff605bb1
paritosh singh <paritosh.singh@team.neustar>
2021-08-11 10:46:35 +0530

    Added address match along with customisable input

--- scripts/batch_ciapi.py
+++ scripts/batch_ciapi.py
@@ -30,12 +30,26 @@ def main():
     NSR_SERVICE_ID = os.environ['NSR_SERVICE_ID']
     NSR_USR = os.environ['NSR_USR'] 
     NSR_PWD = os.environ['NSR_PWD']
+    
+    # creating schema mapping , customizable for client
+    src_schema = {}
+    src_schema['EXTERNAL_ID'] = os.environ['SRC_EXTERNAL_ID']
+    src_schema['EMAIL_COL'] = os.environ['SRC_EMAIL_COL']
+    src_schema['PHONE_COL'] = os.environ['SRC_PHONE_COL']
+    src_schema['FNAME_COL'] = os.environ['SRC_FNAME_COL']
+    src_schema['MNAME_COL'] = os.environ['SRC_MNAME_COL']
+    src_schema['LNAME_COL'] = os.environ['SRC_LNAME_COL']
+    src_schema['ADDR1_COL'] = os.environ['SRC_ADDR1_COL']
+    src_schema['ADDR2_COL'] = os.environ['SRC_ADDR2_COL']
+    src_schema['CITY_COL'] = os.environ['SRC_CITY_COL']
+    src_schema['STATE_COL'] = os.environ['SRC_STATE_COL']
+    src_schema['ZIP_COL'] = os.environ['SRC_ZIP_COL']
 
 
     # initialise client and con
     con = td.connect(apikey=TD_API_KEY, endpoint=TD_API_SERVER)
     client = pytd.Client(apikey=TD_API_KEY, endpoint=TD_API_SERVER, database=PROFILES_DATABASE)
-    cdp_record = get_profiles_pii_in_td(client, PROFILES_TABLE)
+    cdp_record = get_profiles_pii_in_td(client, PROFILES_TABLE, src_schema)
     
     # initialise empty df
     enriched_data = pd.DataFrame(columns=['cdp_customer_id','ekey', 'hhid', 'firstNameMatch', 'middleNameMatch', 'lastNameMatch',
@@ -49,8 +63,8 @@ def main():
         df = json_normalize(data=result['6544'], record_path=['individuals'])
         #prepend the cdp_customer_id to the NSR data for easy profile deduplication later
         df.insert(0,'cdp_customer_id', row[0])
-        # drop sensitive fields {client may drop more based upon their need}
-        df.drop(columns = ['deceased'])
+        # drop sensitive fields as below {client may drop based upon their need}
+        # df.drop(columns = ['deceased'])
         enriched_data = enriched_data.append(df)
 
     
@@ -58,24 +72,33 @@ def main():
     load_data_into_td(con, enriched_data, PROFILES_DATABASE, ENRICHED_PROFILES_TABLE)
 
 ## Functions ##
+def xstr(s):
+    if s is None:
+        return ''
+    return str(s)
 
 # TD standard table where all  profiles are located = customers
-def get_profiles_pii_in_td (client, profiles_tbl):
-    sql = """SELECT cdp_customer_id, firstname, lastname, middleinitial, email, phone1
-    FROM {0} WHERE email is not null OR phone1 IS NOT NULL""".format(profiles_tbl)
+# customise for client specific schema
+def get_profiles_pii_in_td (client, profiles_tbl, schema):
+    sql = """SELECT {1}, {2}, {3}, {4}, {5}, {6}, {7}, {8}, {9}, {10}, {11}
+    FROM {0} WHERE email is not null OR phone1 IS NOT NULL""".format(profiles_tbl, schema['EXTERNAL_ID'], schema['FNAME_COL'], schema['LNAME_COL'], schema['MNAME_COL'], schema['EMAIL_COL'], schema['PHONE_COL'], schema['ADDR1_COL'], schema['ADDR2_COL'], schema['CITY_COL'], schema['STATE_COL'], schema['ZIP_COL'] )
     profiles = client.query(query = sql)
     return profiles
 
-def resolve_identity_from_neustar (usr, pwd, sid, firstname, lastname, middleinitial, email, phone):
-    url = "https://webgwy.neustar.biz/v2/access/query?elems=6544&serviceid={0}&1={1}&572={2}&1395={3},{4},{5}".format(sid, phone, email, firstname, lastname, middleinitial)
+def resolve_identity_from_neustar (usr, pwd, sid, firstname, lastname, middleinitial, email, phone, address1, address2, city, state, zip):
+    # create full address from address lines
+    address = xstr(address1) + " " + xstr(address2)
+    url = "https://webgwy.neustar.biz/v2/access/query?elems=6544&1601=Email=3,Individual,Name,Household,Address&serviceid={0}&1={1}&572={2}&1395={3},{4},{5}&1390={6}&1391={7}&1392={8}&1393={9}".format(sid, phone, email, firstname, lastname, middleinitial, address, city, state, zip)
     payload = ""
     headers = {
         'Content-Type': 'application/json',
         'X-Accept': 'json'
         }
-        
+    print("URL:" + url) 
     response = requests.request("GET", url, headers=headers, data=payload, auth=(usr, pwd))
-    return json.loads(response.text)['response'][0] #LATER : this will become a list for batch queries to the CIAPI
+    try:
+        string_to_return = json.loads(response.text)['response'][0]
+    return  #LATER : this will become a list for batch queries to the CIAPI
 
 # use pandas dataframe method to load table
 # consider using incremental data loads for heavier volumes
