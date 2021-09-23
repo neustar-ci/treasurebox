9d44c0f91f4b89ffc91d5feea859f748e3da8f58
paritosh singh <paritosh.singh@team.neustar>
2021-09-23 13:29:09 +0530

    Added incremental handling of API calls

--- scripts/batch_ciapi.py
+++ scripts/batch_ciapi.py
@@ -49,7 +49,7 @@ def main():
     con = td.connect(apikey=TD_API_KEY, endpoint=TD_API_SERVER)
     client = pytd.Client(apikey=TD_API_KEY, endpoint=TD_API_SERVER, database=PROFILES_DATABASE)
     # now get all profiles for enrichment
-    cdp_record = get_profiles_pii_in_td(client, PROFILES_TABLE, src_schema)
+    cdp_record = get_profiles_pii_in_td(client, PROFILES_TABLE, ENRICHED_PROFILES_TABLE, src_schema)
     
     # initialise empty df
     enriched_data = pd.DataFrame(columns=['cdp_customer_id','ekey', 'hhid', 'firstNameMatch', 'lastNameMatch', 'addressMatches',
@@ -121,9 +121,9 @@ def xstr(s):
 
 # TD standard table where all  profiles are located = customers
 # customise for client specific schema
-def get_profiles_pii_in_td (client, profiles_tbl, schema):
-    sql = """SELECT {1}, {2}, {3}, {4}, {5}, {6}, {7}, {8}, {9}, {10}, {11}
-    FROM {0} WHERE {4} IS NOT NULL or {2} IS NOT NULL""".format(profiles_tbl, schema['EXTERNAL_ID'], schema['FNAME_COL'], schema['LNAME_COL'], schema['MNAME_COL'], schema['PHONE_COL'],schema['EMAIL_COL'], schema['ADDR1_COL'], schema['ADDR2_COL'], schema['CITY_COL'], schema['STATE_COL'], schema['ZIP_COL'] )
+def get_profiles_pii_in_td (client, profiles_tbl, enriched_profiles_tbl, schema):
+    sql = """SELECT a.{1}, a.{2}, a.{3}, a.{4}, a.{5}, a.{6}, a.{7}, a.{8}, a.{9}
+    FROM {0} a left join {10} b on a.{1}=b.cdp_customer_id WHERE b.cdp_customer_id is null AND {4} IS NOT NULL or {2} IS NOT NULL limit 1500""".format(profiles_tbl, schema['EXTERNAL_ID'], schema['FNAME_COL'], schema['LNAME_COL'], schema['MNAME_COL'], schema['PHONE_COL'],schema['EMAIL_COL'], schema['ADDR1_COL'], schema['ADDR2_COL'], schema['CITY_COL'], schema['STATE_COL'], schema['ZIP_COL'], enriched_profiles_tbl )
     #log generated sql , modify if necessary
     print(sql)
     profiles = client.query(query = sql)
@@ -146,7 +146,7 @@ def resolve_identity_from_neustar (usr, pwd, sid, firstname, lastname, middleini
 # use pandas dataframe method to load table
 # consider using incremental data loads for heavier volumes
 def load_data_into_td (client, df, profiles_db, enriched_profiles_tbl):
-    client.load_table_from_dataframe(df, '{0}.{1}'.format(profiles_db, enriched_profiles_tbl), writer = 'bulk_import', if_exists='overwrite')  
+    client.load_table_from_dataframe(df, '{0}.{1}'.format(profiles_db, enriched_profiles_tbl), writer = 'bulk_import', if_exists='append')  
 
 if __name__ == "__main__":
     main()
